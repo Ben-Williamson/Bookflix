5ea75751bb4e6fb1811dce70774ef4a9dbda12f3
Ben Williamson <benmacwill@gmail.com>
2021-07-04 21:22:21 +0100

    First Commit.
    First Commit:  Very basic implementation of a WebServer in MicroPython.

---
+++ boot.py
@@ -0,0 +1,29 @@
+from wifi import connectToNetwork
+from time import sleep
+import network
+
+
+def clear():
+    print("\x1B\x5B2J", end="")
+    print("\x1B\x5BH", end="")
+
+
+clear()
+
+
+accessPoint = network.WLAN(network.AP_IF)
+accessPoint.active(True)
+accessPoint.config(essid="Hamster Tracker", password="***REMOVED***")
+
+client = network.WLAN(network.STA_IF)
+client.active(True)
+client.config(dhcp_hostname="Hamster Tracker")
+client.connect("Williamson WiFi", "***REMOVED***")
+
+
+print("Connecting", end="")
+while not client.isconnected():
+    print(".", end="")
+    sleep(0.25)
+print("")
+print(client.ifconfig())
