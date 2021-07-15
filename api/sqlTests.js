7c165ff1409e6ff50978ea40018d0ba74edf6bbf
Ben Williamson <benmacwill@gmail.com>
2021-07-15 22:45:18 +0000

    removed passwords

--- api/sqlTests.js
+++ api/sqlTests.js
@@ -23,7 +23,6 @@ function addUser(username, password, email) {
 
     });
 }
-addUser("ben", "Gjba1976", "benmacwill@gmail.com");
 
 function comparePassword(username, password) {
     var sql = `SELECT * FROM users WHERE username='${username}'`;
@@ -33,5 +32,3 @@ function comparePassword(username, password) {
         });
     });
 }
-
-comparePassword("ben", "Gjba176")
