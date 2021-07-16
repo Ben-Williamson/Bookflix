f9731093675f52848340bd0a2a819a61c584d7b3
Ben Williamson <benmacwill@gmail.com>
2021-07-16 14:38:44 +0000

    allowed cors requests in testing.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 
 function App() {
+  fetch("http://192.168.0.21:3000/").then((data) => {
+    console.log(data.body);
+  })
+
+
   return (
     <div className="App">
       This is the app
